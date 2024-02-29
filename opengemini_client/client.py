e5922af54d660b3c81857ebe192536c5321c0145
heiliuchao <heiliuchao110@163.com>
2024-02-29 21:21:32 +0800

    【首次提交】

---
+++ opengemini_client/client.py
@@ -0,0 +1,30 @@
+
+"""
+client module
+"""
+from abc import ABC,abstractmethod
+
+import client_impl
+from point import BatchPoints
+from query import Query
+from query_result import QueryResult
+from rentention_policy import RetentionPolicy
+
+
+class Address:
+    """
+    Address configuration for providing service
+    """
+
+    def __init__(self,host: str, port: int):
+        """
+        :param host: service ip or domain.
+        :param port: exposed service port
+        """
+        self.host = host
+        self.port = port
+
+class AuthType:
+    """
+    AuthType reprensents the type of identity authentication
+    """
