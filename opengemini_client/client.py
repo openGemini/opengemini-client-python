563afc112c69c5951193ca091ed2964a604fa636
heiliuchao <heiliuchao110@163.com>
2024-03-02 20:07:20 +0800

    chore: define the client abstract class
    Signed-off-by: heiliuchao <heiliuchao110@163.com>
    Co-authored-by: ZhangJian He <shoothzj@gmail.com>

--- opengemini_client/client.py
+++ opengemini_client/client.py
@@ -2,10 +2,8 @@
 client module
 """
 from abc import ABC, abstractmethod
-from opengemini_client.common import RpCconfig
-from opengemini_client.point import BatchPoints
-from opengemini_client.query import Query
-from opengemini_client.retention_policy import RetentionPolicy
+
+from opengemini_client.models import BatchPoints, QueryResult
 
 
 class Client(ABC):
@@ -22,10 +20,10 @@ class Client(ABC):
         """
 
     @abstractmethod
-    def query(self, query: Query) -> tuple[Query, str]:
+    def query(self, query: object) -> tuple[QueryResult, str]:
         """
         query result
-        :param query: query object
+        :param query: Query object
         :return: returns a two-tuple. the first one is the query result and the second is an error message
         """
 
@@ -34,59 +32,6 @@ class Client(ABC):
         """
         batch points to assigned database
         :param database:  name
-        :param batch_points: Batch points object
+        :param batch_points: BatchPoints object
         :return: return an error message
-        """
-
-    @abstractmethod
-    def create_database(self, database: str) -> str:
-        """
-        create database
-        :param database:  name
-        :return: return an error message of string type
-        """
-
-    @abstractmethod
-    def create_database_with_rp(self, database: str, rp_config: RpCconfig) -> str:
-        """
-        create database with retention policy
-        :param database: database need to create
-        :param rp_config: confi
-        :return: return an error message of string type
-        """
-
-    @abstractmethod
-    def show_database(self, string: list, error: str) -> None:
-        """
-        show database
-        :param string:  list of type String
-        :param error:  error
-        :return: no return value
-        """
-
-    @abstractmethod
-    def drop_database(self, database: str) -> str:
-        """
-        drop database
-        :param database:  name
-        :return: return an error message of string type
-        """
-
-    @abstractmethod
-    def create_retention_policy(self, database: str, rp_config: RpCconfig, is_default: bool) -> str:
-        """
-        create retention policy
-        :param database: database name
-        :param rp_config: configuration information for retention policy
-        :param is_default: set the new retention policy as the default retention policy for the database
-        :return: return an error message of string type
-        """
-
-    @abstractmethod
-    def show_retention_policy(self, database: str) -> tuple[list[RetentionPolicy], str]:
-        """
-        show retention policy
-        :param database: database name
-        :return: return a two-tuple. the first one is a list whose elements are the retention policy
-         and the second is an error message
         """
---
+++ opengemini_client/utils.py
@@ -0,0 +1,15 @@
+import threading
+
+
+class AtomicInt:
+    def __init__(self, value=0):
+        self._value = value
+        self._lock = threading.Lock()
+
+    def increment(self):
+        with self._lock:
+            self._value += 1
+
+    def get_value(self):
+        with self._lock:
+            return self._value
