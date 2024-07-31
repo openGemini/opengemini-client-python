2a8aeee501f4c176ff0d536d73aeeb8d1e1839a2
yh <944207160@qq.com>
2024-08-01 06:42:59 +0800

    fix: add init packages and optimize initialization parameters (#56)
    Signed-off-by: yehao <944207160@qq.com>

---
+++ opengemini_client/__init__.py
@@ -0,0 +1,20 @@
+from .client_impl import OpenGeminiDBClient as Client
+
+from .models import (
+    Address,
+    AuthConfig,
+    AuthType,
+    BatchConfig,
+    BatchPoints,
+    Config,
+    KeyValue,
+    Point,
+    Precision,
+    Query,
+    QueryResult,
+    RetentionPolicy,
+    RpConfig,
+    Series,
+    SeriesResult,
+    ValuesResult
+)
