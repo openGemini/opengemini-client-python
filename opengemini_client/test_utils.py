0818565314e435c02f7ca17012a5d41d48a8ec4d
yh <944207160@qq.com>
2024-08-26 22:10:09 +0800

    feat: optimize client initialization and reduce required parameters (#64)
    Signed-off-by: yehao <944207160@qq.com>

--- opengemini_client/test_utils.py
+++ opengemini_client/test_utils.py
@@ -1,15 +1,8 @@
-import datetime
-
 from opengemini_client import client_impl
 from opengemini_client import models
 
 
 def get_test_default_client():
-    cfg = models.Config(address=[models.Address(host='127.0.0.1', port=8086)],
-                        batch_config=models.BatchConfig(batch_size=10, batch_interval=10),
-                        timeout=datetime.timedelta(seconds=10),
-                        connection_timeout=datetime.timedelta(seconds=10),
-                        gzip_enabled=False, tls_enabled=False
-                        )
+    cfg = models.Config(address=[models.Address(host='127.0.0.1', port=8086)])
     cli = client_impl.OpenGeminiDBClient(cfg)
     return cli
